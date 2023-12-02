4804363ccb66c8d62f898b062a363ca969423d85
tanimtorikul <torikulswe@gmail.com>
2023-12-02 11:56:51 +0600

    verified token and implemented axios interceptor

--- src/Layout/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/Layout/Dashboard/ManageUsers/ManageUsers.jsx
@@ -7,11 +7,7 @@ const ManageUsers = () => {
   const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
-      const res = await axiosSecure.get("/users", {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem("access-token")}`
-        },
-      });
+      const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,34 @@
 import axios from "axios";
 
- const axiosSecure = axios.create({
+const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  // request interceptor to add authorization header for every secure call to api
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("request stopped by interceptor", token);
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  // intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      const status = error.response.status;
+      console.log("status error in the interceptor", status);
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
