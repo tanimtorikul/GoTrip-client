6c32e84b31d5c56deb327207fcfc57af9d1c26b2
tanimtorikul <torikulswe@gmail.com>
2023-12-10 16:07:46 +0600

    fixed responsiveness of navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -5,18 +5,20 @@ import { Toaster } from "react-hot-toast";
 
 const MainLayout = () => {
   return (
-    <div className="font-jost px-4 md:px-4 lg:px-0">
+    <div className="font-jost ">
       <div>
         <Navbar></Navbar>
       </div>
 
-      <div className="min-h-screen py-12 md:py-24">
+     <div className="px-4 md:px-4 lg:px-0">
+     <div className="min-h-screen py-12 md:py-24 ">
         <Outlet></Outlet>
       </div>
 
       <div>
         <Footer></Footer>
       </div>
+     </div>
       <Toaster />
     </div>
   );
